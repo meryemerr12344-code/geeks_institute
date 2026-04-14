326217cce2ee9753feaa75de0e365b0d501e810a
code meryemerr12344 <meryemerr12344@gmail.com>
2026-04-14 16:05:10 +0100

    fix structure: keep only week7/day4

--- week5/day3/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getProducts } = require('../controllers/productController');
-
-const router = express.Router();
-
-// GET /api/products?page=1
-router.get('/', getProducts);
-
-module.exports = router;
