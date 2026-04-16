818d893ce0961c7509e8c63f9dcf74d069b02973
code meryemerr12344 <meryemerr12344@gmail.com>
2026-04-16 12:55:10 +0100

    Initial commit: PostgreSQL exercises

---
+++ week5/day4/Exercises XP.sql
@@ -0,0 +1,32 @@
+-- Active: 1776337587435@@localhost@5432@my_app
+CREATE DATABASE my_app;
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    price INTEGER
+);
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+INSERT INTO items (name, price) VALUES
+('Small Desk', 100),
+('Large Desk', 300),
+('Fan', 80);
+
+INSERT INTO customers (first_name, last_name) VALUES
+('John', 'Doe'),
+('Jane', 'Smith'),
+('Emily', 'Johnson');
+
+SELECT * FROM items;
+SELECT * FROM items WHERE price > 80;
+SELECT * FROM items WHERE price <= 300;
+
+SELECT * FROM customers WHERE last_name = 'Smith';
+SELECT * FROM customers WHERE last_name = 'Johnson';
+
+SELECT * FROM customers WHERE first_name <> 'John';
